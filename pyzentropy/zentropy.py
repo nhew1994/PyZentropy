f77812e8168a5fd20e192bce185f0b6b3333d79c
nhew1994 <nigelhew94@gmail.com>
2024-05-05 09:53:28 -0400

    Update

--- pyzentropy/zentropy.py
+++ pyzentropy/zentropy.py
@@ -25,67 +25,81 @@ def load_phonon_dos(path):
             header=None,
             names=["Frequency (Hz)", "DOS (1/Hz)"],
         )
-        df.insert(0, 'Volume (Å³/atom)', volph_content)
+        df.insert(0, "Volume (Å³/atom)", volph_content)
         dataframes.append(df)
 
     vdos_data = pd.concat(dataframes)
 
     os.chdir(original_path)
-    
+
     return vdos_data
 
-#TODO: Rewrite this function
+
 def scale_phonon_dos(path, num_atoms=5, plot=False):
-    """Scales the area under the phonon DOS to 3N, where N is the number of atoms.
+    """
+    Scales the area under the phonon DOS to 3N, where N is the number of atoms.
     YPHON normalizes the area to 3N.
     """
 
     vdos_data = load_phonon_dos(path)
-    volumes_per_atom = list(vdos_data.keys())
-    vdos_data_scaled = {}
 
-    # Remove the negative frequencies
-    for volume_per_atom in volumes_per_atom:
-        vdos_data_scaled[volume_per_atom] = vdos_data[volume_per_atom][
-            vdos_data[volume_per_atom]["Frequency (Hz)"] > 0
-        ]
-        vdos_data_scaled[volume_per_atom] = vdos_data_scaled[volume_per_atom]
-        new_row = pd.DataFrame([[0, 0]], columns=["Frequency (Hz)", "DOS (1/Hz)"])
-        vdos_data_scaled[volume_per_atom] = pd.concat(
-            [new_row, vdos_data_scaled[volume_per_atom]]
-        ).reset_index(drop=True)
+    # Remove all of the negative frequencies
+    vdos_data_scaled = vdos_data[vdos_data["Frequency (Hz)"] > 0].reset_index(drop=True)
 
-    # Scale the area under the phonon DOS.
+    # Add a row of zero frequency and DOS to the beginning of each volume
+    volumes = np.sort(vdos_data["Volume (Å³/atom)"].unique())
+    final_df = pd.DataFrame(columns=vdos_data_scaled.columns)
+    for volume in volumes:
+        filtered_df = vdos_data_scaled[vdos_data_scaled["Volume (Å³/atom)"] == volume]
+        new_row = pd.DataFrame([[volume, 0, 0]], columns=vdos_data_scaled.columns)
+        filtered_df = pd.concat([new_row, filtered_df])
+        if final_df.empty:
+            final_df = filtered_df
+        else:
+            final_df = pd.concat([final_df, filtered_df])
+    vdos_data_scaled = final_df.reset_index(drop=True)
+
+    # Scale the phonon DOS to 3N
     num_atoms_3N = num_atoms * 3
-    for volume_per_atom in volumes_per_atom:
-        area = np.trapz(
-            vdos_data_scaled[volume_per_atom]["DOS (1/Hz)"],
-            vdos_data_scaled[volume_per_atom]["Frequency (Hz)"],
-        )
-        vdos_data_scaled[volume_per_atom]["DOS (1/Hz)"] *= num_atoms_3N / area
+    for volume in volumes:
+        frequency = vdos_data_scaled[vdos_data_scaled["Volume (Å³/atom)"] == volume][
+            "Frequency (Hz)"
+        ]
+        dos = vdos_data_scaled[vdos_data_scaled["Volume (Å³/atom)"] == volume][
+            "DOS (1/Hz)"
+        ]
+        area = np.trapz(dos, frequency)
+        vdos_data_scaled.loc[
+            vdos_data_scaled["Volume (Å³/atom)"] == volume, "DOS (1/Hz)"
+        ] = (dos * num_atoms_3N / area)
 
     if plot == True:
-        for volume_per_atom in volumes_per_atom:
-            plt.plot(
-                vdos_data[volume_per_atom]["Frequency (Hz)"],
-                vdos_data[volume_per_atom]["DOS (1/Hz)"],
-                label="original",
-            )
-            plt.plot(
-                vdos_data_scaled[volume_per_atom]["Frequency (Hz)"],
-                vdos_data_scaled[volume_per_atom]["DOS (1/Hz)"],
-                label="scaled",
-            )
+        for volume in volumes:
+            # Plot the original DOS
+            frequency = vdos_data[vdos_data["Volume (Å³/atom)"] == volume][
+                "Frequency (Hz)"
+            ]
+            dos = vdos_data[vdos_data["Volume (Å³/atom)"] == volume]["DOS (1/Hz)"]
+            plt.plot(frequency, dos, label="Original")
+
+            # Plot the scaled DOS
+            frequency = vdos_data_scaled[
+                vdos_data_scaled["Volume (Å³/atom)"] == volume
+            ]["Frequency (Hz)"]
+            dos = vdos_data_scaled[vdos_data_scaled["Volume (Å³/atom)"] == volume][
+                "DOS (1/Hz)"
+            ]
+            plt.plot(frequency, dos, label=f"Scaled to {num_atoms} atoms")
             plt.xlabel("Frequency (Hz)")
             plt.ylabel("DOS (1/Hz)")
-            title = f"Scaled to {round(volume_per_atom * num_atoms, 2)} Å³/{num_atoms} atoms"
-            plt.title(title)
             plt.legend(edgecolor="black")
+            plt.title(f"Volume: {volume} Å³/atom")
             plt.show()
 
     return vdos_data_scaled
 
 
+# TODO: Rewrite this function
 def plot_phonon_dos(path, scale_atoms=5, save_plot=True):
 
     vdos_data_scaled = scale_phonon_dos(path, num_atoms=scale_atoms)
@@ -192,17 +206,25 @@ def harmonic(path, temp_range, plot=True):
         ] = internal_energy
         harmonic_properties[volume_per_atom]["Entropy (eV/K/atom)"] = entropy
         harmonic_properties[volume_per_atom]["Heat Capacity (eV/K/atom)"] = cv
-    
+
     if plot == True:
-        properties = [('Free Energy (eV/atom)', 'Free Energy (eV/atom)'), ('Entropy (eV/K/atom)', 'Entropy (eV/K/atom)'), ("Heat Capacity (eV/K/atom)", "Heat Capacity (eV/K/atom)")]
+        properties = [
+            ("Free Energy (eV/atom)", "Free Energy (eV/atom)"),
+            ("Entropy (eV/K/atom)", "Entropy (eV/K/atom)"),
+            ("Heat Capacity (eV/K/atom)", "Heat Capacity (eV/K/atom)"),
+        ]
         for property_name, ylabel in properties:
             plt.figure()
             for volume_per_atom in volumes_per_atom:
-                plt.plot(temp_range, harmonic_properties[volume_per_atom][property_name], label = f"{volume_per_atom} (Å³/atom)")
-            plt.legend(edgecolor = 'black')
-            plt.xlabel('Temperature (K)')
-            plt.ylabel(ylabel)  
-        
+                plt.plot(
+                    temp_range,
+                    harmonic_properties[volume_per_atom][property_name],
+                    label=f"{volume_per_atom} (Å³/atom)",
+                )
+            plt.legend(edgecolor="black")
+            plt.xlabel("Temperature (K)")
+            plt.ylabel(ylabel)
+
     return vdos_data_scaled, harmonic_properties
 
 
@@ -213,72 +235,162 @@ def eosfitall(volume, energy, m, n):
 
     if m == 1:  # mBM: modified Birch-Murnaghan
         if n == 2:
-            A1 = np.hstack((np.ones(volume.shape), volume**(-1 / 3)))
-            A2 = np.hstack(
-                (np.ones(volume_range.shape), volume_range**(-1 / 3)))
+            A1 = np.hstack((np.ones(volume.shape), volume ** (-1 / 3)))
+            A2 = np.hstack((np.ones(volume_range.shape), volume_range ** (-1 / 3)))
         elif n == 3:
-            A1 = np.hstack((np.ones(volume.shape), volume **
-                           (-1 / 3), volume**(-2 / 3)))
-            A2 = np.hstack((np.ones(volume_range.shape),
-                           volume_range**(-1 / 3), volume_range**(-2 / 3)))
+            A1 = np.hstack(
+                (np.ones(volume.shape), volume ** (-1 / 3), volume ** (-2 / 3))
+            )
+            A2 = np.hstack(
+                (
+                    np.ones(volume_range.shape),
+                    volume_range ** (-1 / 3),
+                    volume_range ** (-2 / 3),
+                )
+            )
         elif n == 4:
-            A1 = np.hstack((np.ones(volume.shape), volume **
-                           (-1 / 3), volume**(-2 / 3), volume**(-1)))
-            A2 = np.hstack((np.ones(volume_range.shape), volume_range **
-                           (-1 / 3), volume_range**(-2 / 3), volume_range**(-1)))
+            A1 = np.hstack(
+                (
+                    np.ones(volume.shape),
+                    volume ** (-1 / 3),
+                    volume ** (-2 / 3),
+                    volume ** (-1),
+                )
+            )
+            A2 = np.hstack(
+                (
+                    np.ones(volume_range.shape),
+                    volume_range ** (-1 / 3),
+                    volume_range ** (-2 / 3),
+                    volume_range ** (-1),
+                )
+            )
         elif n == 5:
-            A1 = np.hstack((np.ones(volume.shape), volume**(-1 / 3),
-                           volume**(-2 / 3), volume**(-1), volume**(-4 / 3)))
-            A2 = np.hstack((np.ones(volume_range.shape), volume_range**(-1 / 3),
-                           volume_range**(-2 / 3), volume_range**(-1), volume_range**(-4 / 3)))
+            A1 = np.hstack(
+                (
+                    np.ones(volume.shape),
+                    volume ** (-1 / 3),
+                    volume ** (-2 / 3),
+                    volume ** (-1),
+                    volume ** (-4 / 3),
+                )
+            )
+            A2 = np.hstack(
+                (
+                    np.ones(volume_range.shape),
+                    volume_range ** (-1 / 3),
+                    volume_range ** (-2 / 3),
+                    volume_range ** (-1),
+                    volume_range ** (-4 / 3),
+                )
+            )
 
     elif m == 2:  # BM: Birch-Murnaghan
         if n == 2:
-            A1 = np.hstack((np.ones(volume.shape), volume**(-2 / 3)))
-            A2 = np.hstack(
-                (np.ones(volume_range.shape), volume_range**(-2 / 3)))
+            A1 = np.hstack((np.ones(volume.shape), volume ** (-2 / 3)))
+            A2 = np.hstack((np.ones(volume_range.shape), volume_range ** (-2 / 3)))
         elif n == 3:
-            A1 = np.hstack((np.ones(volume.shape), volume **
-                           (-2 / 3), volume**(-4 / 3)))
-            A2 = np.hstack((np.ones(volume_range.shape),
-                           volume_range**(-2 / 3), volume_range**(-4 / 3)))
+            A1 = np.hstack(
+                (np.ones(volume.shape), volume ** (-2 / 3), volume ** (-4 / 3))
+            )
+            A2 = np.hstack(
+                (
+                    np.ones(volume_range.shape),
+                    volume_range ** (-2 / 3),
+                    volume_range ** (-4 / 3),
+                )
+            )
         elif n == 4:
-            A1 = np.hstack((np.ones(volume.shape), volume **
-                           (-2 / 3), volume**(-4 / 3), volume**(-2)))
-            A2 = np.hstack((np.ones(volume_range.shape), volume_range **
-                           (-2 / 3), volume_range**(-4 / 3), volume_range**(-2)))
+            A1 = np.hstack(
+                (
+                    np.ones(volume.shape),
+                    volume ** (-2 / 3),
+                    volume ** (-4 / 3),
+                    volume ** (-2),
+                )
+            )
+            A2 = np.hstack(
+                (
+                    np.ones(volume_range.shape),
+                    volume_range ** (-2 / 3),
+                    volume_range ** (-4 / 3),
+                    volume_range ** (-2),
+                )
+            )
         elif n == 5:
-            A1 = np.hstack((np.ones(volume.shape), volume**(-2 / 3),
-                           volume**(-4 / 3), volume**(-2), volume**(-8 / 3)))
-            A2 = np.hstack((np.ones(volume_range.shape), volume_range**(-2 / 3),
-                           volume_range**(-4 / 3), volume_range**(-2), volume_range**(-8 / 3)))
+            A1 = np.hstack(
+                (
+                    np.ones(volume.shape),
+                    volume ** (-2 / 3),
+                    volume ** (-4 / 3),
+                    volume ** (-2),
+                    volume ** (-8 / 3),
+                )
+            )
+            A2 = np.hstack(
+                (
+                    np.ones(volume_range.shape),
+                    volume_range ** (-2 / 3),
+                    volume_range ** (-4 / 3),
+                    volume_range ** (-2),
+                    volume_range ** (-8 / 3),
+                )
+            )
 
     elif m == 3:  # LOG
         if n == 2:
             A1 = np.hstack((np.ones(volume.shape), np.log(volume)))
             A2 = np.hstack((np.ones(volume_range.shape), np.log(volume_range)))
         elif n == 3:
-            A1 = np.hstack(
-                (np.ones(volume.shape), np.log(volume), np.log(volume)**2))
-            A2 = np.hstack((np.ones(volume_range.shape), np.log(
-                volume_range), np.log(volume_range)**2))
+            A1 = np.hstack((np.ones(volume.shape), np.log(volume), np.log(volume) ** 2))
+            A2 = np.hstack(
+                (
+                    np.ones(volume_range.shape),
+                    np.log(volume_range),
+                    np.log(volume_range) ** 2,
+                )
+            )
         elif n == 4:
-            A1 = np.hstack((np.ones(volume.shape), np.log(
-                volume), np.log(volume)**2, np.log(volume)**3))
-            A2 = np.hstack((np.ones(volume_range.shape), np.log(
-                volume_range), np.log(volume_range)**2, np.log(volume_range)**3))
+            A1 = np.hstack(
+                (
+                    np.ones(volume.shape),
+                    np.log(volume),
+                    np.log(volume) ** 2,
+                    np.log(volume) ** 3,
+                )
+            )
+            A2 = np.hstack(
+                (
+                    np.ones(volume_range.shape),
+                    np.log(volume_range),
+                    np.log(volume_range) ** 2,
+                    np.log(volume_range) ** 3,
+                )
+            )
         elif n == 5:
-            A1 = np.hstack((np.ones(volume.shape), np.log(volume), np.log(
-                volume)**2, np.log(volume)**3, np.log(volume)**4))
-            A2 = np.hstack((np.ones(volume_range.shape), np.log(volume_range), np.log(
-                volume_range)**2, np.log(volume_range)**3, np.log(volume_range)**4))
+            A1 = np.hstack(
+                (
+                    np.ones(volume.shape),
+                    np.log(volume),
+                    np.log(volume) ** 2,
+                    np.log(volume) ** 3,
+                    np.log(volume) ** 4,
+                )
+            )
+            A2 = np.hstack(
+                (
+                    np.ones(volume_range.shape),
+                    np.log(volume_range),
+                    np.log(volume_range) ** 2,
+                    np.log(volume_range) ** 3,
+                    np.log(volume_range) ** 4,
+                )
+            )
 
     eos_parameters = np.linalg.pinv(A1).dot(energy)
     energy_fit = np.dot(A2, eos_parameters)
 
     return eos_parameters.T, energy_fit.T
-
-
 
 
 # 3. Quasiharmonic phonon calculations -
